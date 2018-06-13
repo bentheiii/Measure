cb3773ed9ae8a664c577b3a07f8ea532f918304a
Ben Avrahami <avrahami.ben@gmail.com>
2018-06-13 23:37:08 +0300

    cleanup

--- measure/__init__.py
+++ measure/__init__.py
@@ -3,5 +3,5 @@ from .measure import Measure, MutableMeasure, BasicMeasure, AggregateMeasure, Ba
 from .linear import LinearMeasure, Ladder
 from .dynamic import DynamicMeasure
 
-__version__ = '0rc1.dev2'
+__version__ = '0.1'
 __author__ = 'Ben Avrahami'
